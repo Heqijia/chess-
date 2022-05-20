e222ff27b67114390d097c09ace10b46bf2663c1
86159 <gjy20030112@163.com>
2022-05-21 00:45:29 +0800

    bless u

--- src/view/Chessboard.java
+++ src/view/Chessboard.java
@@ -3,11 +3,14 @@ package view;
 import controller.ClickController;
 import model.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * 这个类表示面板上的棋盘组件对象
@@ -34,8 +37,12 @@ public class Chessboard extends JComponent {
     private final int CHESS_SIZE;
     private JLabel jLabel;
     private JLabel jLabelTime;
+    private String Image1;
+    private String Image2;
     public List<String> regretChessDataHelp=new ArrayList<String >();
     public List<String> regretChessDataTure;
+    private Color[] ChessBoardColor;
+//    java.util.Timer timer =new Timer();
 
 //    public Chessboard(List<String> regretChessDataTure) {
 //       regretChessDataTure=this.regretChessDataTure;
@@ -161,12 +168,14 @@ public class Chessboard extends JComponent {
     }
 
     public void swapChessComponents(ChessComponent chess1, ChessComponent chess2) {
-        //JOptionPane.showMessageDialog(this, "游戏结束，恭喜白方获得胜利！");
+
         // Note that chess1 has higher priority, 'destroys' chess2 if exists.
         if (!(chess2 instanceof EmptySlotComponent)) {
             remove(chess2);
             add(chess2 = new EmptySlotComponent(chess2.getChessboardPoint(), chess2.getLocation(), clickController, CHESS_SIZE));
+
         }
+
         chess1.swapLocation(chess2);
         int row1 = chess1.getChessboardPoint().getX(), col1 = chess1.getChessboardPoint().getY();
         chessComponents[row1][col1] = chess1;
@@ -187,15 +196,9 @@ public class Chessboard extends JComponent {
         //System.out.println(regretChessDataTure+"++++++++=================");
         chess1.repaint();
         chess2.repaint();
-//        Object[] options1={"骑士","皇后","大象","战车"};
-//        int op1 = JOptionPane.showOptionDialog(null, "请选择升级成：", "升级",JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE,null, options1, options1[0]);
         regretChessDataHelp=StoreGame();
 
         if (regretChessDataHelp.get(7).contains("P")){
-            //System.out.println(")))))))))))))))))))))))))))))))))))))))))))))");
-
-//                System.out.println(regretChessDataHelp.get(7));
-//            System.out.println(regretChessDataHelp.get(7).charAt(1));
             for(int j=0;j<8;j++) {
                 if (regretChessDataHelp.get(7).charAt(j) == 'P') {
                     //System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
@@ -267,8 +270,15 @@ public class Chessboard extends JComponent {
                         (regretChessDataHelp.get(2).charAt(j)=='p')&&
                         (regretChessDataHelp.get(3).charAt(j)=='P')){
                     remove(chessComponents[3][j]);
-                    add(new EmptySlotComponent(new ChessboardPoint(3,j),calculatePoint(3,j),clickController,CHESS_SIZE));
+                    //changeChessBoard(path1,path2);
+
+                    //add(new EmptySlotComponent(new ChessboardPoint(3,j),calculatePoint(3,j),clickController,CHESS_SIZE));
+                    ChessComponent _=new EmptySlotComponent(new ChessboardPoint(3,j),calculatePoint(3,j),clickController,CHESS_SIZE);
+                    _.setVisible(true);
+                    putChessOnBoard(_);
+                    //changeChessBoard(path1,path2);
                 }
+
             }
         for (int j=0;j<8;j++){
             if((regretChessDataTure.get(5).charAt(j)=='_')&&
@@ -276,9 +286,16 @@ public class Chessboard extends JComponent {
                     (regretChessDataHelp.get(5).charAt(j)=='P')&&
                     (regretChessDataHelp.get(4).charAt(j)=='p')){
                 remove(chessComponents[4][j]);
-                add(new EmptySlotComponent(new ChessboardPoint(4,j),calculatePoint(4,j),clickController,CHESS_SIZE));
+                //changeChessBoard(path1,path2);
+                ChessComponent _=new EmptySlotComponent(new ChessboardPoint(4,j),calculatePoint(4,j),clickController,CHESS_SIZE);
+                _.setVisible(true);
+                putChessOnBoard(_);
+                //changeChessBoard(path1,path2);
             }
         }
+
+
+
         if(jiangsi(chessComponents)){
             if(currentColor==ChessColor.BLACK){
                 JOptionPane.showMessageDialog(this, "游戏结束，恭喜白方获得胜利！");
@@ -286,7 +303,15 @@ public class Chessboard extends JComponent {
                 JOptionPane.showMessageDialog(this, "游戏结束，恭喜黑方获得胜利！");
             }
         }
+        if(jiangsiCase2(chessComponents)){
+            if(currentColor==ChessColor.BLACK){
+                JOptionPane.showMessageDialog(this, "游戏结束，恭喜黑方获得胜利！");
+            }else {
+                JOptionPane.showMessageDialog(this, "游戏结束，恭喜白方获得胜利！");
+            }
+        }
 
+//        changeChessBoard(path1,path2);
 
 
     }
@@ -300,7 +325,6 @@ public class Chessboard extends JComponent {
     }
 
     public void swapColor() {
-
         currentColor = currentColor == ChessColor.BLACK ? ChessColor.WHITE : ChessColor.BLACK;
         if (currentColor==ChessColor.BLACK){
             jLabel.setText("        黑方回合");
@@ -310,25 +334,8 @@ public class Chessboard extends JComponent {
         regretChessDataTure=regretChessDataHelp;
         regretChessDataHelp=StoreGame();
         //getTime();
+        time3();
 
-    }
-
-    public void getTime(){
-        long seconds=20;
-        while(seconds>=0){
-            jLabelTime.setText("剩余"+seconds+"秒");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            seconds--;
-            if(currentColor==ChessColor.WHITE){
-                currentColor=ChessColor.BLACK;
-            }else {
-                currentColor=ChessColor.WHITE;
-            }
-        }
     }
 
     public void setRegretChessDataTure(List<String> regretChessDataTure) {
@@ -366,11 +373,11 @@ public class Chessboard extends JComponent {
         putChessOnBoard(chessComponent);
     }
 
-//    @Override
-////    protected void paintComponent(Graphics g) {
-////        super.paintComponent(g);
-////        ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-////    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    }
 
     private Point calculatePoint(int row, int col) {
         return new Point(col * CHESS_SIZE, row * CHESS_SIZE);
@@ -476,9 +483,9 @@ public class Chessboard extends JComponent {
     public void changeLabel(JLabel jLabel){
         this.jLabel=jLabel;
     }
-    public void changeTime(JLabel jLabelTime){
-        this.jLabelTime=jLabelTime;
-    }
+//   // public void changeTime(JLabel jLabelTime){
+//        this.jLabelTime=jLabelTime;
+//    }
 
 
     public boolean jiangsi(ChessComponent[][] chessComponents) {
@@ -486,11 +493,11 @@ public class Chessboard extends JComponent {
         int y0 = 0;
         int judge1 = -1;
         int judge2 = -1;
-        int juege4 = -1;
+        int judge4 = 0;
         int counter = 0;
-        int judge3=0;
-        //找到对手的王的位置(x0,y0)，判断对手是否输棋是在我下完棋之后瞬间判断（原先还没有swap chesscomponent，所以判断也是失效的。
-        //找到王的位置：
+        int judge3 = 0;
+        //鎵惧埌瀵规墜鐨勭帇鐨勪綅缃?x0,y0)锛屽垽鏂鎵嬫槸鍚﹁緭妫嬫槸鍦ㄦ垜涓嬪畬妫嬩箣鍚庣灛闂村垽鏂紙鍘熷厛杩樻病鏈塻wap chesscomponent锛屾墍浠ュ垽鏂篃鏄け鏁堢殑銆?
+        //鎵惧埌鐜嬬殑浣嶇疆锛?
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (chessComponents[i][j].getChessColor() == currentColor && chessComponents[i][j].getChessColor() != null &&
@@ -500,20 +507,17 @@ public class Chessboard extends JComponent {
                 }
             }
         }
-        //王无论如何行棋或者不行棋都会输：
+        //鐜嬫棤璁哄浣曡妫嬫垨鑰呬笉琛屾閮戒細杈擄細
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (chessComponents[i][j].getChessColor() != currentColor&&
-                        chessComponents[i][j].getChessColor() !=ChessColor.NONE) {
+                if (chessComponents[i][j].getChessColor() != currentColor &&
+                        chessComponents[i][j].getChessColor() != ChessColor.NONE) {
                     List<ChessboardPoint> points = chessComponents[i][j].getPointsCanMoveTo(this.getChessComponents());
                     // List<ChessboardPoint> points=new ArrayList<ChessboardPoint>();
 
                     //for(int k=0;k<points.size();k++) {
                     for (int l = 0; l < chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).size(); l++) {
                         for (int k = 0; k < points.size(); k++) {
-                            if (points.get(k).getX() == x0 && points.get(k).getY() == y0) {
-                                judge1 = 1;//确保王在攻击范围之内
-                            }
                             if (points.get(k).getX() == chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).get(l).getX() &&
                                     points.get(k).getY() == chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).get(l).getY()) {
                                 counter++;
@@ -529,47 +533,191 @@ public class Chessboard extends JComponent {
         }
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (chessComponents[i][j].getChessColor() != currentColor&&
-                chessComponents[i][j].getChessColor() != ChessColor.NONE) {
+                if (chessComponents[i][j].getChessColor() != currentColor &&
+                        chessComponents[i][j].getChessColor() != ChessColor.NONE) {
                     List<ChessboardPoint> points = chessComponents[i][j].getPointsCanMoveTo(this.getChessComponents());
                     for (int k = 0; k < points.size(); k++) {
-                        if (points.get(k).getX() == x0 && points.get(k).getY() == y0) {//记得造成威胁的点是points.get(k)错！这是国王的点chessComponents[i][j]才是
+                        if (points.get(k).getX() == x0 && points.get(k).getY() == y0) {//璁板緱閫犳垚濞佽儊鐨勭偣鏄痯oints.get(k)閿欙紒杩欐槸鍥界帇鐨勭偣chessComponents[i][j]鎵嶆槸
                             judge3++;
-                            label:for(int l=0;l<8;l++){
-                                for(int m=0;m<8;m++){
-                                    System.out.println(chessComponents[l][m].getChessColor()+"))");
+                            label:
+                            for (int l = 0; l < 8; l++) {
+                                for (int m = 0; m < 8; m++) {
+                                    //System.out.println(chessComponents[l][m].getChessColor()+"))");
+                                    //int count=0;
                                     if (chessComponents[l][m].getChessColor() == currentColor &&
-                                            chessComponents[l][m].getChessColor() != ChessColor.NONE){
+                                            chessComponents[l][m].getChessColor() != ChessColor.NONE) {
                                         //System.out.print("_");System.out.println(currentColor);
-                                        //System.out.print(i); System.out.println(j);
-                                        for(int n=0;n<chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).size();n++){
-                                            if(chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).get(n).getX()==i&&
-                                               chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).get(n).getY()==j){
-                                                //System.out.println("++++++++");
+                                        // System.out.print(i); System.out.println(j);
+                                        for (int n = 0; n < chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).size(); n++) {
+//                                            count++;
+//                                            System.out.println(count);
+                                            if (chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).get(n).getX() == i &&
+                                                    chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).get(n).getY() == j) {
+                                                System.out.println("++++++++");
                                                 judge3--;
-
+                                                break label;
                                             }
                                         }
-                                        break label;
                                     }
-                                    //break;
                                 }
-                                //break;
                             }
                         }
                     }
                 }
             }
         }
-
 //System.out.println(x0+" "+y0);
 //System.out.println(judge1+"***"+judge2+"***"+judge3+"***");
-        if ( judge2 == 1 && judge3 != 0) {
+        if (judge2 == 1 && judge3 != 0) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+    public boolean jiangsiCase2(ChessComponent[][] chessComponents){
+        int x0 = 0;
+        int y0 = 0;
+        int judge1 = -1;
+        int judge2 = -1;
+        int judge4 = 0;
+        int counter = 0;
+        int judge3 = 0;
+        //鎵惧埌瀵规墜鐨勭帇鐨勪綅缃?x0,y0)锛屽垽鏂鎵嬫槸鍚﹁緭妫嬫槸鍦ㄦ垜涓嬪畬妫嬩箣鍚庣灛闂村垽鏂紙鍘熷厛杩樻病鏈塻wap chesscomponent锛屾墍浠ュ垽鏂篃鏄け鏁堢殑銆?
+        //鎵惧埌鐜嬬殑浣嶇疆锛?
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (chessComponents[i][j].getChessColor() != currentColor && chessComponents[i][j].getChessColor() != null &&
+                        (chessComponents[i][j].getName() == "K" || chessComponents[i][j].getName() == "k")) {
+                    x0 = i;
+                    y0 = j;
+                }//找到了我的王
+            }
+        }
+        //鐜嬫棤璁哄浣曡妫嬫垨鑰呬笉琛屾閮戒細杈擄細
+        for (int i = 0; i < 8; i++) {//判断对面的棋子（currentColor）能不能覆盖我的王的全部的位置
+            for (int j = 0; j < 8; j++) {
+                if (chessComponents[i][j].getChessColor() == currentColor &&
+                        chessComponents[i][j].getChessColor() != ChessColor.NONE) {
+                    List<ChessboardPoint> points = chessComponents[i][j].getPointsCanMoveTo(this.getChessComponents());
+                    // List<ChessboardPoint> points=new ArrayList<ChessboardPoint>();
+
+                    //for(int k=0;k<points.size();k++) {
+                    for (int l = 0; l < chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).size(); l++) {
+                        for (int k = 0; k < points.size(); k++) {
+                            if (points.get(k).getX() == chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).get(l).getX() &&
+                                    points.get(k).getY() == chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).get(l).getY()) {
+                                counter++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (counter == chessComponents[x0][y0].getPointsCanMoveTo(this.getChessComponents()).size()) {
+            judge2 = 1;//能够覆盖我的王的全部位置，那么为1这时候已经死了一半了
+        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (chessComponents[i][j].getChessColor() == currentColor &&
+                        chessComponents[i][j].getChessColor() != ChessColor.NONE) {
+                    List<ChessboardPoint> points = chessComponents[i][j].getPointsCanMoveTo(this.getChessComponents());
+                    for (int k = 0; k < points.size(); k++) {
+                        if (points.get(k).getX() == x0 && points.get(k).getY() == y0) {///找到对手对我的王构成威胁的点：i，j处
+                            judge3++;
+                            label:
+                            for (int l = 0; l < 8; l++) {
+                                for (int m = 0; m < 8; m++) {
+                                    //System.out.println(chessComponents[l][m].getChessColor()+"))");
+                                    if (chessComponents[l][m].getChessColor() != currentColor &&
+                                            chessComponents[l][m].getChessColor() != ChessColor.NONE) {
+                                        //System.out.print("_");System.out.println(currentColor);
+                                        // System.out.print(i); System.out.println(j);
+                                        for (int n = 0; n < chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).size(); n++) {
+                                            if (chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).get(n).getX() == i &&
+                                                    chessComponents[l][m].getPointsCanMoveTo(this.getChessComponents()).get(n).getY() == j) {
+                                                judge3--;
+                                                break label;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+//System.out.println(x0+" "+y0);
+//System.out.println(judge1+"***"+judge2+"***"+judge3+"***");
+        if (judge2 == 1 && judge3 != 0) {
             return true;
         } else {
             return false;
         }
-    }//判断结束
+    }
+public void changeChessBoard(String Image1,String Image2){
+        this.Image1=Image1;
+        this.Image2=Image2;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                try {
+                    chessComponents[i][j].setImage1(ImageIO.read(new File(this.Image1)));
+                    chessComponents[i][j].setImage2(ImageIO.read(new File(this.Image2)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //chessComponents[i][j].setImage1(ImageIO.read(new File(this.Image1)));
+            }
+        }
+        repaint();
+}
+
+    public void changeChessBoardColor(Color[] ChessBoardColor){
+       this.ChessBoardColor=ChessBoardColor;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                //chessComponents[i][j].setBACKGROUND_COLORS(ChessBoardColor);
+            }
+        }
+        repaint();
+    }
 
 
+    public void time3() {
+        ChessColor bb=currentColor;
+        final int[] midTime = {50};
+//        timer=new Timer();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                midTime[0]--;
+//                long hh = midTime[0] / 60 / 60 % 60;
+//                long mm = midTime[0] / 60 % 60;
+                long ss = midTime[0] % 60;
+                jLabelTime.setText("还剩"+ss+"秒");
+                //System.out.println("还剩" + ss + "秒");
+                if(currentColor!=bb){
+                    timer.cancel();
+                }
+                if(ss==0){
+                    swapColor();
+                    timer.cancel();
+                }
+            }
+
+        }, 0, 1000);
+    }
+
+    public void changeTime(JLabel label){
+        this.jLabelTime=label;
+    }
 }
